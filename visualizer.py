59c7021db4ff11dd54bc51699e86683cbe086f6e
Milos Milosevic <milos.milosevic@tomtom.com>
2023-02-28 12:27:17 +0100

    visualize start

--- visualizer.py
+++ visualizer.py
@@ -3,7 +3,10 @@
 import argparse
 import json
 import matplotlib.pyplot as plt
-from datetime import datetime
+import numpy as np
+
+
+from datetime import datetime, timedelta
 
 DATATIME_FORMAT = '%d.%m.%Y %H:%M:%S.%f'
 
@@ -27,8 +30,8 @@ CANCEL_TRIP_PLANNING_REQUEST = 'Cancel Trip planning requested ??????'
 # errors
 PROTOBUF_CHANNEL_INTERUPTED_ERR = 'Protobuf channel interrupted'
 
-EMPTY_TRIP = {'id':None, 'plan':None, 'routes':[], 'create':None, 'response':None, 'navigated':None, 'better_proposals':[], 'delete':None}
-EMPTY_CRASH = {'pid':None, 'tid':None, 'cmd':None, 'stacktrace':'', 'ts':None, 'tracetime':None}
+EMPTY_TRIP = {'id':None, 'plan':None, 'routes':[], 'created':None, 'response':None, 'navigated':None, 'better_proposals':[], 'deleted':None}
+EMPTY_CRASH = {'pid':None, 'tid':None, 'cmd':None, 'stacktrace':'', 'timestamp':None, 'uptime':None, 'tracetime':None}
 
 def setup_parser():
     # Setup CLI arguments
@@ -74,6 +77,7 @@ for line in input_file:
   if packet_id < args.start:
       continue
 
+  # Crash staff
   if current_crash != None:
     if CRASH_DEBUG in line:
       crash_skips = 0
@@ -94,11 +98,15 @@ for line in input_file:
 
       if 'Timestamp:' in line:
         ts_str = line[line.find('Timestamp:')+11:].rstrip()
-        current_crash['ts'] = datetime.strptime(ts_str[:25] + ts_str[29:], '%Y-%m-%d %H:%M:%S.%f%z')
+        current_crash['timestamp'] = datetime.strptime(ts_str[:25] + ts_str[29:], '%Y-%m-%d %H:%M:%S.%f%z')
+        continue
+
+      if 'Process uptime:' in line:
         continue
 
       if 'backtrace:' in line:
         backtrace_step = 0
+        continue
 
     else:
       # It can happen that other process print inbetween crash process
@@ -117,10 +125,12 @@ for line in input_file:
     current_crash['tracetime'] = get_tracetime(line)
     continue
 
+
+  # Plan trip staff
   if PLAN_TRIP_REQUEST in line:
     assert current_trip == None
     current_trip = EMPTY_TRIP
-    current_trip['tracetime'] = get_tracetime(line)
+    current_trip['created'] = get_tracetime(line)
     current_trip['id'] = get_tid(line, PLAN_TRIP_REQUEST)
     current_trip['plan'] = line[line.find('TripPlan'):]
     continue
@@ -136,8 +146,53 @@ for line in input_file:
     assert current_trip != None
     assert get_tid(line, DELETE_TRIP_RESPONSE) == current_trip['id']
     current_trip['deleted'] = get_tracetime(line)
-    trip_data[current_trip['id']] = current_trip
+    trip_data[current_trip['id']] = current_trip.copy()
     current_trip = None
     continue
 
-print(trip_data)
+for tid, trip in trip_data.items():
+  labels = []
+  dates = []
+
+  for key in ['created', 'navigated', 'deleted']:
+    if trip.get(key) != None:
+      labels.append('{0:%H:%m:%s}\n{1}'.format(trip.get(key), key))
+      dates.append(trip.get(key))
+
+  min_date = np.min(dates) - timedelta(hours=0, minutes=1)
+  max_date = np.max(dates) + timedelta(hours=0, minutes=1)
+
+  fig, ax = plt.subplots(figsize=(15, 4), constrained_layout=True)
+  _ = ax.set_ylim(-2, 1.75)
+  _ = ax.set_xlim(min_date, max_date)
+  _ = ax.axhline(0, xmin=0.05, xmax=0.95, c='deeppink', zorder=1)
+
+  _ = ax.scatter(dates, np.zeros(len(dates)), s=120, c='palevioletred', zorder=2)
+  _ = ax.scatter(dates, np.zeros(len(dates)), s=30, c='darkmagenta', zorder=3)
+
+  label_offsets = np.zeros(len(dates))
+  label_offsets[::2] = 0.35
+  label_offsets[1::2] = -0.7
+  for i, (l, d) in enumerate(zip(labels, dates)):
+      _ = ax.text(d, label_offsets[i], l, ha='center', fontfamily='serif', fontweight='bold', color='royalblue',fontsize=12)
+
+  stems = np.zeros(len(dates))
+  stems[::2] = 0.3
+  stems[1::2] = -0.3
+  markerline, stemline, baseline = ax.stem(dates, stems, use_line_collection=True)
+  _ = plt.setp(markerline, marker=',', color='darkmagenta')
+  _ = plt.setp(stemline, color='darkmagenta')
+
+
+  # hide lines around chart
+  for spine in ["left", "top", "right", "bottom"]:
+      _ = ax.spines[spine].set_visible(False)
+
+  # hide tick labels
+  _ = ax.set_xticks([])
+  _ = ax.set_yticks([])
+
+  _ = ax.set_title('Trip {}'.format(tid), fontweight="bold", fontfamily='serif', fontsize=16,
+                   color='royalblue')
+
+  plt.show()
