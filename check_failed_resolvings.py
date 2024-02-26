296768c46873ed5a3a0e04fd56538fdf5c8e0473
Milos Milosevic <milos.milosevic@tomtom.com>
2024-02-26 12:11:18 +0100

    Add log line for LogRouteSummary

--- check_failed_resolvings.py
+++ check_failed_resolvings.py
@@ -8,6 +8,7 @@ SEPARATOR = "\n\n---------------------------------------------------------------
 ONLINE_REQ_REGEX = "SendOnlineRequest.*Route/"
 RECEIVING_ROUTE_REGEX = "Receiving.*online route"
 RESOLVING_FAILED_PHRASE = "Polyline resolving failed for route"
+LOG_ROUTE_SUMMART_PHRASE = "LogRouteSummary"
 
 def setup_parser():
     # Setup CLI arguments
@@ -31,6 +32,7 @@ output_file = open(output_file_name, "w")
 
 last_request = None
 last_id = None
+last_route_summary = None
 cnt = 0
 
 for line in input_file:
@@ -38,11 +40,22 @@ for line in input_file:
     last_request = line
   elif re.search(RECEIVING_ROUTE_REGEX, line, re.IGNORECASE):
     last_id = line
+  elif LOG_ROUTE_SUMMART_PHRASE in line:
+    last_route_summary = line
   elif RESOLVING_FAILED_PHRASE in line:
+    if not(last_request and last_id and last_route_summary):
+      print("Some of the failures not paired properly, the log format might have changed")
+
     output_file.write(last_request)
+    output_file.write(last_route_summary)
     output_file.write(last_id)
     output_file.write(line)
     output_file.write(SEPARATOR)
+
+    last_request = None
+    last_id = None
+    last_route_summary = None
+
     cnt = cnt + 1
 
 print("Detected {} polyline resolvings failed".format(cnt))
